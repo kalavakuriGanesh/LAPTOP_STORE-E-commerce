30376c58377d79fc8c4b256e8c50da74a3745732
kalavakuriGanesh <156083416+kalavakuriGanesh@users.noreply.github.com>
2024-03-25 23:12:47 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,114 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Lenovo Thinkpad",
+    img: "img/product-1.jpeg",
+    price: 55000,
+    company: "LENOVO",
+    info:
+      "Lenovo laptops are designed to suit different needs and budgets, from work to gaming to entertainment123. Lenovo offers high-performance laptops for creators, professionals, or gamers that can compete with desktops. Lenovo also has ultraportable, thin, and light laptops for people who are always on the go",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "DELL LATITUDE E-5420",
+    img: "img/product-2.jpeg",
+    price: 90000,
+    company: "DELL",
+    info:
+      "12th Generation Intel® Core™ i5-1235 Windows 11 Home Single Languag Intel® Iris® Xe Graphics 16 GB: 2 x 8 GB, DDR4, 2666 MT/ 512 GB, M.2, PCIe NVMe, SSd 39.6-cm. display Full HD (1920X1080) Starting at 1.65 kg ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "ASUS VIVOBOOK S14",
+    img: "img/product-3.jpeg",
+    price: 80000,
+    company: "ASUS VIVOBOOK",
+    info:
+      "14.5-inch 120Hz 2.8K OLED 550nits HDR peak brightness Intel Core i5-12500H Intel Evo Platform 16 GB RAM Intel Iris Xe Graphics 512GB M.2 NVMe PCIe 4.0 SSD Sandy Grey Windows 11 Home.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HP 14S",
+    img: "img/product-4.jpeg",
+    price: 38000,
+    company: "HP",
+    info:
+      "Processor: 12th Generation Intel® Core™ i5 RAM: 16 GB DDR4 Storage: 512 GB SSD Graphics: Intel® Iris® Xᵉ Graphics Display: 14-inch diagonal Brightview Features: Backlit keyboard Fingerprint reader Wide Vision 720p HD camera B&O Speakers",
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "BOAT EARBUDS",
+    img: "img/product-5.jpeg",
+    price: 1999,
+    company: "BOAT",
+    info:
+      "boAt Airdopes 141 Bluetooth TWS Earbuds with 42H Playtime,Low Latency Mode for Gaming, ENx Tech, IWP, IPX4 Water Resistance, Smooth Touch Controls(Bold Black)",
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Iphone 4s",
+    img: "img/product-6.png",
+    price: 4000,
+    company: "Apple",
+    info:
+      "The iPhone 4S runs on the A5 chip which allows you to multitask seamlessly and ensures that the iOS runs without hitches. The software of the phone is designed to take full advantage of the hardware, thereby allowing you to have a fully optimized, smooth experience..",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Samsung Galaxy Book 4 pro",
+    img: "img/product-7.jpeg",
+    price: 85000,
+    company: "Samsung",
+    info:
+      "Samsung Galaxy Book4 Pro (Moonstone Gray, 16GB RAM, 512GB SSD)| 14 Dynamic AMOLED 2X Touchscreen| Intel Core Ultra 5 125H Processor| Windows 11 Home| MS Office 2021| Fingerprint Reader.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Honor",
+    img: "img/product-8.jpeg",
+    price: 48000,
+    company: "HONOR",
+    info:
+      "HONOR MagicBook X16 (2024), 12th Gen Intel Core i5-12450H, 16-inch (40.64 cm) FHD IPS Anti-Glare Thin and Light Laptop (16GB/512GB PCIe SSD/Windows 11/ Full-Size Numeric Keyboard /1.68Kg), Gray",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
